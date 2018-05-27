e7cb33de9ec928a0d488ed6255d38cdb404659e9
AlexNguyenDev <39652898+AlexNguyenDev@users.noreply.github.com>
2018-05-27 13:00:04 +0700

    Update Pokerhand.py

--- Pokerhand.py
+++ Pokerhand.py
@@ -7,27 +7,27 @@ def check_card(cards):
         else:
             pass
     return 1
-def check_suit(suits):
-    suit_value=['H','C','D','S']
-    for c in suits:
-        if c not in suit_value:
+def check_rank(ranks):
+    rank_value=['H','C','D','S']
+    for c in ranks:
+        if c not in rank_value:
             return 0
         else:
             pass
     return 1
 def check_string(check_value):
-    suit_value=['H','C','D','S']
+    rank_value=['H','C','D','S']
     check=bool(re.search(r'\s',check_value))
     if check == True: 
         return 2
     else:
-        if check_value[0] not in suit_value:
+        if check_value[0] not in rank_value:
             return 1
         else: return 0
 def desk(hand):
     checks_string=check_string(hand)# Check Input String Format
     cards = []
-    suits = []
+    ranks = []
     list_covert=list(hand)
     if checks_string == 2:
         return "Error Format String!!!"
@@ -37,16 +37,16 @@ def desk(hand):
                 index = list_covert.index(l)
                 if index % 2 == 0:
                     cards.append(l)
-                else: suits.append(l)
+                else: ranks.append(l)
         else:
             for l in list_covert:
                 index = list_covert.index(l)
                 if index % 2 == 0:
-                    suits.append(l)
+                    ranks.append(l)
                 else: cards.append(l)
         check_cards=check_card(cards)# Check Card
-        check_suits=check_suit(suits)# Check Suit
-        if check_cards == 1 and check_suits == 1: 
+        check_ranks=check_rank(ranks)# Check Suit
+        if check_cards == 1 and check_ranks == 1: 
             same_cards = sorted([cards.count(a) for a in set(cards)])
             if len(same_cards) == 2:
                 if max(same_cards) == 4: # Four Cards
@@ -62,7 +62,7 @@ def desk(hand):
                 return "1P"
             else: # No hands
                 return "-------"
-        else: return "Error card or suit...."       
+        else: return "Error card or rank...."       
 
 if __name__ == "__main__":
     hand=input('input string poker hand: ')
